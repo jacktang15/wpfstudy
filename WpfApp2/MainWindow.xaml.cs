196090d04a1579beca3ff419fc9f24e0c7cb7133
jacktang15 <jacktang1507@gmail.com>
2023-11-17 20:30:43 +0800

    add ellipse example code

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApp2
         public MainWindow()
         {
             InitializeComponent();
-            DrawCirclePattern();
+            DrawPanelCirclePattern();
         }
 
         private void ClickIt_Click(object sender, RoutedEventArgs e)
@@ -47,28 +47,28 @@ namespace WpfApp2
         private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (CurrELP != null)
-            {
-                CurrELP.Stroke = Brushes.Transparent;
-                // CurrELP.StrokeThickness = 1;
+            {                
+                CurrELP.Stroke = Brushes.Gray;
+                CurrELP.StrokeThickness = 2;
             }
 
 
             Ellipse clickedEllipse = (Ellipse)sender;
-            // clickedEllipse.Fill = Brushes.Black;
-            clickedEllipse.Stroke = Brushes.Red;
-            clickedEllipse.StrokeThickness = 1;
+            clickedEllipse.Stroke = Brushes.Blue;
+            clickedEllipse.StrokeThickness = 4;
                         
             CurrELP = clickedEllipse;
-            // DrawHalfCircle((Ellipse)sender);
+            // DrawHalfCircle(clickedEllipse);
+            // Console.WriteLine(clickedEllipse.Name);
         }
 
         private void DrawHalfCircle(Ellipse ellipse)
-        {           
-
+        {
+            var aw = ellipse.ActualWidth;
             PathGeometry pathGeometry = new PathGeometry();
-            ArcSegment arcSegment = new ArcSegment(new Point(35, 70), new Size(35, 35), 1, false, SweepDirection.Clockwise, true);
+            ArcSegment arcSegment = new ArcSegment(new Point(aw/2, aw), new Size(aw/2, aw/2), 1, false, SweepDirection.Clockwise, true);
             PathFigure pathFigure = new PathFigure();
-            pathFigure.StartPoint = new Point(36, 3);
+            pathFigure.StartPoint = new Point(aw/2, 0);
             pathFigure.Segments.Add(arcSegment);
             pathGeometry.Figures.Add(pathFigure);
 
@@ -87,6 +87,52 @@ namespace WpfApp2
                 ellipse.Margin = new Thickness(5);
 
                 st1.Children.Add(ellipse);
+            }
+        }
+
+        private void DrawPanelCirclePattern()
+        {
+            for (int l = 0; l < 2; l++) {
+                StackPanel inner2StackPanel = new StackPanel();
+                inner2StackPanel.Orientation = Orientation.Horizontal;
+                inner2StackPanel.Margin = new Thickness(5);
+
+                for (int k = 0; k < 3; k++) {
+                    StackPanel inner1StackPanel = new StackPanel();
+                    inner1StackPanel.Orientation = Orientation.Vertical;
+                    inner1StackPanel.Margin = new Thickness(5);
+
+                    for (int i = 0; i < 4; i++)
+                    {
+                        StackPanel innerStackPanel = new StackPanel();
+                        innerStackPanel.Orientation = Orientation.Horizontal;
+                        innerStackPanel.Margin = new Thickness(2);
+
+                        for (int j = 0; j < 4; j++)
+                        {
+                            Ellipse ellipse = new Ellipse();
+                            ellipse.Width = 20;
+                            ellipse.Height = 20;
+                            if (i == 2 && j == 1) {
+                                ellipse.Fill = Brushes.LightBlue;
+                            } else { 
+                                ellipse.Fill = Brushes.LightGray;
+                            }
+                            ellipse.Stroke = Brushes.Gray;
+                            ellipse.StrokeThickness = 2;
+                            ellipse.Margin = new Thickness(2);
+
+                            ellipse.MouseDown += Ellipse_MouseDown;
+                            ellipse.Name = "elp" + (l+1) + (k+1) + (i + 1) + (j + 1);
+
+                            innerStackPanel.Children.Add(ellipse);
+                        }
+
+                        inner1StackPanel.Children.Add(innerStackPanel);
+                    }
+                    inner2StackPanel.Children.Add(inner1StackPanel);
+                }
+                outerStackPanel.Children.Add(inner2StackPanel);
             }
         }
     }
